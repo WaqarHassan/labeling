8d4f9d8a47f44551e36b89f45c1f0904ee2021ae
hussain <hussain@hussains-MacBook.local>
2016-08-15 15:38:14 +0500

    ui changes

--- app/assets/javascripts/overview.js
+++ app/assets/javascripts/overview.js
@@ -48,48 +48,6 @@ function overview_serach_box()
 	  }
 }
 
-function overview_serach_box_reserve()
-{
-     var className = $('.project-formset').attr('class');
-      if (className=='project-data project-formset collapsed'){
-      	$('#labeling_body').removeClass('labeling_body_detail');
-      	 $('#labeling_container').removeClass('labeling_container_detail');
-
-      	 $('#labeling_body').addClass('labeling_body_detail_search');
-      	 $('#labeling_container').addClass('labeling_container_detail_search');
-
-		 $('#projectform').removeClass('collapsed');
-		 $('#detail_data').removeClass('tspend_initial');
-
-		 var classNameIadiv = $('.project_ia').attr('class');
-		 if (classNameIadiv=='project-data project_ia collapsed'){
-		 	$('#project_ia').addClass('project_serach_noDetail');
-		 }
-
-	  }else{
-	  	 $('#labeling_body').removeClass('labeling_body_detail_search');
-      	 $('#labeling_container').removeClass('labeling_container_detail_search');
-      	 $('#project_ia').removeClass('project_serach_noDetail');
-
-      	 $('#labeling_body').addClass('labeling_body_detail_search');
-      	 $('#labeling_container').addClass('labeling_container_detail_search');
-
-		 $('#projectform').addClass('collapsed');
-		 $('#detail_data').addClass('tspend');
-
-		 var className = $('.project-formset').attr('class');
-     	 var className_datil = $('.project_ia').attr('class');
-	 	 if ( (className_datil == "project-data project_ia collapsed" || className_datil == 'project-data project_ia collapsed project_serach_noDetail') && className == 'project-data project-formset collapsed'){
-	        $('#labeling_body').removeClass('labeling_body_detail');	
-		  	 $('#labeling_container').removeClass('labeling_container_detail');
-		  	 $('#labeling_body').removeClass('labeling_body_detail_search');
-	      	 $('#labeling_container').removeClass('labeling_container_detail_search');
-	      	 $('#project_ia').removeClass('project_serach_noDetail');
-	      	 $('#detail_data').addClass('tspend_initial');
-		 }	  
-	  }
-}
-
 function label_attributes_box()
 {
 	 var searchclassName = $('#projectform').attr('class');	
@@ -117,50 +75,6 @@ function label_attributes_box()
 
 		 $('#detail_data').removeClass('search_and_labels_both_hidden');
 	  }
-	  
-	 
+	  	 
 }
 
-function label_attributes_box_reserve()
-{
-
-     var className = $('.project_ia').attr('class');
-
-      if (className == "project-data project_ia collapsed" || className == 'project-data project_ia collapsed project_serach_noDetail'){
-      	 $('#labeling_body').addClass('labeling_body_detail');
-      	 $('#labeling_container').addClass('labeling_container_detail');
-		 $('#project_ia').removeClass('collapsed');
-		 $('.collapse_table_data').removeClass('collapse_table');
-		 $('.collapse_table_data').removeClass('collapse_table');
-		 $('#detail_data').removeClass('tspend');
-		 $('#project_ia').removeClass('project_serach_noDetail');
-		 $('#detail_data').removeClass('tspend_initial');
-	  }else{	
-	  	 $('#labeling_body').removeClass('labeling_body_detail');	
-	  	 $('#labeling_container').removeClass('labeling_container_detail');
-	  	 $('.collapse_table_data').addClass('collapse_table');
-		 $('.collapse_table_data').addClass('collapse_table');
-		 $('#project_ia').addClass('collapsed');
-		 $('#detail_data').addClass('tspend');
-
-		 var classNameSearchdiv = $('.project-formset').attr('class');
-		 if (classNameSearchdiv=='project-data project-formset'){
-		 	$('#project_ia').addClass('project_serach_noDetail');
-		 }
-
-	 	 var className = $('.project_ia').attr('class');
-	     var classNameSearch = $('.project-formset').attr('class');
-
-		 if ( (className == "project-data project_ia collapsed"  || className == 'project-data project_ia collapsed project_serach_noDetail' )&& classNameSearch == 'project-data project-formset collapsed'){
-		  	 $('#labeling_body').removeClass('labeling_body_detail');	
-		  	 $('#labeling_container').removeClass('labeling_container_detail');
-		  	 $('#labeling_body').removeClass('labeling_body_detail_search');
-		  	 $('#labeling_container').removeClass('labeling_container_detail_search');
-		  	 $('#project_ia').removeClass('project_serach_noDetail');
-		  	 $('#detail_data').addClass('tspend_initial');
-		 }
-
-	  }
-	  
-	 
-}

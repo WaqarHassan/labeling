c162c22c900e1365c33edd8992929b9b589a2493
hussain <hussain@hussains-MacBook.local>
2016-08-16 10:04:47 +0500

    change

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -8,7 +8,7 @@ class WorkFlow < ActiveRecord::Base
 			def search(q_string)
 				  serach_result = ActiveRecord::Base.connection.select_all "Select l1s.id as l1_id, l1s.name as l1_name, l2s.id as l2_id, l2s.name as l2_name, l3s.id as l3_id, l3s.name as l3_name 
                     from l1s left join l2s on l1s.id = l2s.l1_id left join l3s on l2s.id = l3s.l2_id 
-                    where #{q_string}"
+                    where #{q_string} order by l1s.name"
                    return serach_result
 			end
 

1aaa8ba8b80bda65f301372443b537f18f597014
hussain <hussain@hussains-MacBook.local>
2016-09-02 14:31:57 +0500

    eta changes

--- app/models/transition.rb
+++ app/models/transition.rb
@@ -1,3 +1,4 @@
 class Transition < ActiveRecord::Base
-	belongs_to :station_step
+	belongs_to :station_step, class_name: "StationStep", foreign_key: "station_step_id"
+	belongs_to :previous_station_step, class_name: "StationStep", foreign_key: "previous_station_step_id"
 end

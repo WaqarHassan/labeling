571cacb03f9a37449f5aa9a88e622e46ed80c9a6
waqar-devbox <waqar.hassan@devbox.co>
2016-09-06 18:17:45 +0500

    width again set

--- app/assets/javascripts/labeling_cookie.js
+++ app/assets/javascripts/labeling_cookie.js
@@ -1,8 +1,8 @@
 if (window.jQuery) {  
 	$(document).ready(function() {
-
+		console.log('inside labeling_Cookie.js')
 		if (Cookies.get('project_ia') && Cookies.get('projectform') ){
-			console.log('cooke present....');
+			console.log('Cookie present and gonna get them');
 			project_ia_classes = Cookies.get('project_ia'); 	 
 			projectform_classes = Cookies.get('projectform'); 
 			
@@ -10,21 +10,38 @@ if (window.jQuery) {
 			$('#projectform').removeClass();
 			$('#project_ia').addClass(project_ia_classes);
 			$('#projectform').addClass(projectform_classes);
+			adjust();
+			// overview_serach_box();
+			// label_attributes_box();
+			// var labelsClassName = $('.project_ia').attr('class');	
+   //   		var className = $('#projectform').attr('class');
+			// if (className=='project-data' && labelsClassName == 'project-data project_ia collapsed project_serach_noDetail'){
+      	 
+			// var detail_data = container_width() - 300 ;
+			// $('#detail_data').width(detail_data);
 
-			overview_serach_box();
-			label_attributes_box();
-			
+   //    		}
+   //    		else if (className=='project-data'){
+			// 	var detail_data = container_width() - 300 - 600;
+			// 	$('#detail_data').width(detail_data);
+
+	  // 		}else if (className=='project-data collapsed' && labelsClassName == 'project-data project_ia collapsed project_serach_noDetail'){
+			// 	var detail_data = container_width() - 300 -250  ;
+			// 	$('#detail_data').width(detail_data);
+			// }else
+			// { 
+			// 		var detail_data = container_width() - 250 - 300 - 600 ;
+			// 		$('#detail_data').width(detail_data);
+	  // 		}
+
 		}
 		else
 		{
-			console.log('============>Labeling cookie .js ')
-			console.log(container_width());
+			console.log('Cookie not present');
 			var detail_data = container_width() - 250 - 300 - 600 ;
+			console.log('detail_data width = '+detail_data);
 			$('#detail_data').width(detail_data);
 		}
-
-		console.log('window.jquery2');
-
 	});
 }
 function container_width(){
@@ -32,7 +49,7 @@ function container_width(){
 }
 function overview_serach_box()
 {
-	console.log('overview called! =>>>>>>>>>>>>>>>>');
+	console.log('inside overview_serach_box');
 	 var labelsClassName = $('.project_ia').attr('class');	
      var className = $('#projectform').attr('class');
 
@@ -58,17 +75,49 @@ function overview_serach_box()
 		var detail_data = container_width() - 250 - 300 - 600 ;
 		$('#detail_data').width(detail_data);
 	  }
-
+	  console.log('=>gonna set Cookies');
 	project_ia_classes = $('#project_ia').attr('class');	 
 	projectform_classes = $('#projectform').attr('class');
 
 	Cookies.set('project_ia', project_ia_classes, { expires: 7 });
-	Cookies.set('projectform', projectform_classes, { expires: 7 });	  
+	Cookies.set('projectform', projectform_classes, { expires: 7 });
+	console.log('===>Cookies are set');	  
 }
+function adjust(){
 
+	if($('#projectform').hasClass('collapsed'))
+	{
+		if($('#project_ia').hasClass('collapsed'))
+		{
+			var detail_data = container_width() - 300 ;
+			$('#detail_data').width(detail_data);	
+		}
+		else{
+			var detail_data = container_width() - 300 - 600 ;
+			$('#detail_data').width(detail_data);
+
+		}
+
+	}
+	else
+	{
+		if($('#project_ia').hasClass('collapsed'))
+		{
+			var detail_data = container_width() -250 - 300 ;
+			$('#detail_data').width(detail_data);	
+		}
+		else{
+			var detail_data = container_width() - 300 - 600 - 250 ;
+			$('#detail_data').width(detail_data);
+
+		}
+
+	}
+
+}
 function label_attributes_box()
 {
-	console.log('label_attribute called! =>>>>>>>>>>>>>>>>');
+	console.log('inside label_attributes_box');
 	 var searchclassName = $('#projectform').attr('class');	
      var className = $('.project_ia').attr('class');
 
@@ -107,11 +156,12 @@ function label_attributes_box()
 		$('#detail_data').width(detail_data);
 
 	  }
-
+	  console.log('=>gonna set Cookies');
 	project_ia_classes = $('#project_ia').attr('class');	 
 	projectform_classes = $('#projectform').attr('class');
 
 	Cookies.set('project_ia', project_ia_classes, { expires: 7 });
 	Cookies.set('projectform', projectform_classes, { expires: 7 });
+	console.log('======>cookies are set');
 	  	 
 }

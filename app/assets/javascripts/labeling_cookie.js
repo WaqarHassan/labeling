4392657e3688d905603a1e1f9323fc96a7d743e4
waqar-devbox <waqar.hassan@devbox.co>
2016-09-06 11:41:12 +0500

    commented code removed

--- app/assets/javascripts/labeling_cookie.js
+++ app/assets/javascripts/labeling_cookie.js
@@ -5,14 +5,11 @@ if (window.jQuery) {
 			console.log('cooke present....');
 			project_ia_classes = Cookies.get('project_ia'); 	 
 			projectform_classes = Cookies.get('projectform'); 
-			//detail_data_classes = Cookies.get('detail_data');
 			
 			$('#project_ia').removeClass();
 			$('#projectform').removeClass();
-			//$('#detail_data').removeClass();
 			$('#project_ia').addClass(project_ia_classes);
 			$('#projectform').addClass(projectform_classes);
-			//$('#detail_data').addClass(detail_data_classes);
 
 
 			label_attributes_box();
--- app/assets/javascripts/overview.js
+++ app/assets/javascripts/overview.js
@@ -24,16 +24,6 @@ function resetSearchResult(){
 
 $(document).ready(function() {
 
-	// 	console.log('sjkadhaj df');
-	// var container_width  = $('#labeling_container').width();
-	// var project_ia = ( container_width * 37 ) / 97;
-	// var detail_data = container_width - 250 - 300 - project_ia;
-	// $('#detail_data.approval-data.tspend_initial').width(detail_data); 
-	// $('#detail_data.approval-data.tspend_initial.labels_hidden').width(detail_data + project_ia); 
-	//$('.approval-data.tspend_initial.labels_hidden').width();
-
-	// console.log('detail_data.labels_hidden = ' + (detail_data + project_ia));
-	// console.log('detail_data = ' + detail_data);
 	
 	$('.modal-dialog').draggable({
 	    handle: ".modal-header"
@@ -43,14 +33,11 @@ $(document).ready(function() {
 		console.log('cooke present....');
 		project_ia_classes = Cookies.get('project_ia'); 	 
 		projectform_classes = Cookies.get('projectform'); 
-		//detail_data_classes = Cookies.get('detail_data');
 		
 		$('#project_ia').removeClass();
 		$('#projectform').removeClass();
-		//$('#detail_data').removeClass();
 		$('#project_ia').addClass(project_ia_classes);
 		$('#projectform').addClass(projectform_classes);
-		//$('#detail_data').addClass(detail_data_classes);
 
 		label_attributes_box();
 		overview_serach_box();
@@ -74,47 +61,32 @@ function overview_serach_box()
 
       if (className=='project-data' && labelsClassName == 'project-data project_ia collapsed project_serach_noDetail'){
       	 $('#projectform').addClass('collapsed');
-		 //$('#detail_data').addClass('search_labels_hidden');
-		 //$('#detail_data').addClass('search_and_labels_both_hidden');
-
-		//var project_ia = ( container_width() * 37 ) / 97;
 		var detail_data = container_width() - 300 ;
 		$('#detail_data').width(detail_data);
 
       }
       else if (className=='project-data'){
 		 $('#projectform').addClass('collapsed');
-		 //$('#detail_data').addClass('search_labels_hidden');
-
-		//var project_ia = (( container_width() * 37 ) / 97) - 2;
 		var detail_data = container_width() - 300 - 600;
 		$('#detail_data').width(detail_data);
 
 	  }else if (className=='project-data collapsed' && labelsClassName == 'project-data project_ia collapsed project_serach_noDetail'){
 	  	$('#projectform').removeClass('collapsed');
-		//$('#detail_data').removeClass('search_labels_hidden');
-		//$('#detail_data').removeClass('search_and_labels_both_hidden');
 		$('#project_ia').addClass('project_serach_noDetail');
-		//$('#detail_data').addClass('labels_hidden'); ===>
 		var detail_data = container_width() - 300 -250  ;
 		$('#detail_data').width(detail_data);
 
 	  }else{
 	  	 $('#projectform').removeClass('collapsed'); 
-	  	//$('#detail_data').removeClass('search_labels_hidden'); 
-	  	 //$('#detail_data').removeClass('search_and_labels_both_hidden');
-		//var project_ia = (( container_width() * 37 ) / 97) -2;
 		var detail_data = container_width() - 250 - 300 - 600 ;
 		$('#detail_data').width(detail_data);
 	  }
 
 	project_ia_classes = $('#project_ia').attr('class');	 
 	projectform_classes = $('#projectform').attr('class');
-	//detail_data_classes = $('#detail_data').attr('class');
 
 	Cookies.set('project_ia', project_ia_classes, { expires: 7 });
-	Cookies.set('projectform', projectform_classes, { expires: 7 });
-	//Cookies.set('detail_data', detail_data_classes, { expires: 7 });	  
+	Cookies.set('projectform', projectform_classes, { expires: 7 });	  
 }
 
 
@@ -133,8 +105,6 @@ function label_attributes_box()
 		 $('.collapse_table_data').addClass('collapse_table');
 		 $('.collapse_table_data').addClass('collapse_table');
 		 $('#project_ia').addClass('project_serach_noDetail');
-
-      	 //$('#detail_data').addClass('search_and_labels_both_hidden');
 		var detail_data = container_width() - 300 ;
 		$('#detail_data').width(detail_data);
 
@@ -144,7 +114,6 @@ function label_attributes_box()
 		 $('.collapse_table_data').addClass('collapse_table');
 		 $('.collapse_table_data').addClass('collapse_table');
 		 $('#project_ia').addClass('project_serach_noDetail');
-		// $('#detail_data').addClass('labels_hidden');
 		 var detail_data = container_width() - 300 - 250  ;
 		$('#detail_data').width(detail_data);
 
@@ -162,9 +131,6 @@ function label_attributes_box()
 		 $('.collapse_table_data').removeClass('collapse_table');
 		 $('.collapse_table_data').removeClass('collapse_table');
 		 $('#project_ia').removeClass('project_serach_noDetail');
-		// $('#detail_data').removeClass('labels_hidden');
-
-		// $('#detail_data').removeClass('search_and_labels_both_hidden');
 		var detail_data = container_width() - 300 - 600 - 250;
 		$('#detail_data').width(detail_data);
 
@@ -172,11 +138,9 @@ function label_attributes_box()
 
 	project_ia_classes = $('#project_ia').attr('class');	 
 	projectform_classes = $('#projectform').attr('class');
-	//detail_data_classes = $('#detail_data').attr('class');
 
 	Cookies.set('project_ia', project_ia_classes, { expires: 7 });
 	Cookies.set('projectform', projectform_classes, { expires: 7 });
-	//Cookies.set('detail_data', detail_data_classes, { expires: 7 });
 	  	 
 }
 

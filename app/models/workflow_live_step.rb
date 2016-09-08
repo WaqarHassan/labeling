305cbb9b3beedc711467975271c8731f6e19fce3
hussain <hussain@hussains-MacBook.local>
2016-09-08 17:03:33 +0500

    eta

--- app/models/workflow_live_step.rb
+++ app/models/workflow_live_step.rb
@@ -98,10 +98,10 @@ class WorkflowLiveStep < ActiveRecord::Base
 	          predecessors_step_ojbets.each_with_index do |pso, indx|
 	            if indx == 0 and pso.step_completion.present?
 	              station_step = wls.station_step
-	              max_step_completion = station_step.calculate_step_completion(pso, pso.step_completion, comp_attribute_value, lang_attribute_value, hours_per_workday)
+	              max_step_completion = station_step.calculate_step_completion(wls, pso.step_completion, comp_attribute_value, lang_attribute_value, hours_per_workday)
 	            elsif pso.step_completion.present?
 	              station_step = wls.station_step
-	              step_completion_other = station_step.calculate_step_completion(pso, pso.step_completion, comp_attribute_value, lang_attribute_value, hours_per_workday)
+	              step_completion_other = station_step.calculate_step_completion(wls, pso.step_completion, comp_attribute_value, lang_attribute_value, hours_per_workday)
 	                if DateTime.parse(step_completion_other.to_s) > DateTime.parse(max_step_completion.to_s)
 	                  max_step_completion = step_completion_other 
 	                end

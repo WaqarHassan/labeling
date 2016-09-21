7b0b7db0a3a0fc40d27bd270c9cd2cb3e9a6601c
waqar-devbox <waqar.hassan@devbox.co>
2016-09-21 17:55:13 +0500

    self == this

--- app/models/l3.rb
+++ app/models/l3.rb
@@ -7,9 +7,9 @@ class L3 < ActiveRecord::Base
 	has_many :additional_info, as: :object
 	has_many :timestamp_logs, through: :workflow_live_steps
 
-	def get_farthest_to_the_right_confirmation(l3_id)
+	def get_farthest_to_the_right_confirmation
 
-		wf_live_step = L3.find_by_id(l3_id).workflow_live_steps.where.not( actual_confirmation: nil).order('id DESC').first
+		wf_live_step = self.workflow_live_steps.where.not( actual_confirmation: nil).order('workflow_live_steps.id DESC').first
 
 			if !wf_live_step.present?
 				wf_live_step = L3.find_by_id(l3_id).workflow_live_steps.first

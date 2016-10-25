1f1dc2b34631a681b46d3cf1961a3255a5aade40
mhussain2 <muhammad.hussain@devbox.co>
2016-10-25 20:28:33 +0500

    ss

--- app/models/work_flow.rb
+++ app/models/work_flow.rb
@@ -1365,9 +1365,9 @@ class WorkFlow < ActiveRecord::Base
 			      		 'BU', 'IA #Comp', 'ECR', 'ECR-Status', 'ECR-Completed', 'ECR #Comp', 'ParentId',
 			      		  'Parent', 'IA Approved', 'ECR Inbox', 'Sent to Collab', 'Back from Collab', 
 			      		  'Station8 Sent', 'CRB Started - ETA', 'CRB Started', 'ECN Released', 'Horw', 
-			      		  '#Lang', 'CompType', 'MainRecord', 'IA-HoldReason','ECR-HoldReason', 'IncludeCompleted', 'Station8 Sent - ETA']
+			      		  '#Lang', 'CompType', 'MainRecord', 'IA-HoldReason','ECR-HoldReason', 'IncludeCompleted','Project-id', 'IA-id', 'ECR-id', 'Station8 Sent - ETA']
 			      data_set.each do |report|
-			        csv <<  [report[0], report[1], report[2], report[3], report[4], report[5], report[6], report[7], report[8], report[9], report[10], report[11], report[12], report[13], report[14], report[15], report[16], report[17], report[18], report[19], report[20], report[21], report[22], report[23], report[24], report[25], report[26], report[27], report[28], report[32]]
+			        csv <<  [report[0], report[1], report[2], report[3], report[4], report[5], report[6], report[7], report[8], report[9], report[10], report[11], report[12], report[13], report[14], report[15], report[16], report[17], report[18], report[19], report[20], report[21], report[22], report[23], report[24], report[25], report[26], report[27], report[28], report[29], report[30], report[31], report[32]]
 			      end
 			    end
 			end
